30d0d92dadb3086274081ada03ad8d1e754f6b63
pedram <pedram@eyelevate.com>
2017-03-25 18:07:10 +0900

    'bug-fixes'

--- platforms/browser/www/js/layouts/popup.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function(){
-	var this_id = document.getElementById("popup_id").value;
-	$('#atwl-btn').attr('data',this_id);
-	$('.fbc').html('');
-	setTimeout(function(){ 
-		HelperFuncions.findAndViewAd(this_id);
-	 }, 500);
-	
-});
