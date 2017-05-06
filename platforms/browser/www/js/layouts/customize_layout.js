c34370bf35c102afdd93b3ceec8216dad6949f0e
pedram <pedram@eyelevate.com>
2017-05-06 20:59:29 +0900

    'icon-and-splash-renewed'

---
+++ platforms/browser/www/js/layouts/customize_layout.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+	cust_layout.pageLoad();
+	cust_layout.events();
+
+});
+cust_layout = {
+	pageLoad: function() {
+	},
+	events: function() {
+	}
+}
+requestws = {
+};
+
---
+++ platforms/browser/www/js/my-app.js
@@ -0,0 +1,19 @@
+// Initialize your app
+var myApp = new Framework7({
+    statusbarOverlay: false
+});
+// Export selectors engine
+var $$ = Dom7;
+
+// Add views
+var view1 = myApp.addView('#view-1');
+var view2 = myApp.addView('#view-2');
+// var view3 = myApp.addView('#view-3');
+var view4 = myApp.addView('#view-4');
+
+
+var mySearchbar = myApp.searchbar('.searchbar', {
+    searchList: '.list-block-search',
+    searchIn: '.item-title'
+});   
+
